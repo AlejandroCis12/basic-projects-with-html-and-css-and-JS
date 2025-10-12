c1dd0ca1109366b3dc999ce45402130803bfae14
AlejandroCis12 <alejandro240915@gmail.com>
2025-10-11 22:09:22 -0600

    proyectos hechos

---
+++ Colores hex/app.js
@@ -0,0 +1,25 @@
+//selecionar los elementos del DOM
+
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
+
+function generarColorH(){
+    let digitos = '0123456789ABCDEF';
+    let colorHex= '#' ;
+
+    for(let i=0;i<6;i++){
+        let indiceAleatorio = Math.floor(Math.random()*16);
+        colorHex += digitos[indiceAleatorio];
+    }
+
+    return colorHex
+}
+
+boton.addEventListener('click',function(){
+    let colorAleatorio = generarColorH();
+    //Actualizar el texto
+    color.textContent = colorAleatorio;
+    //actualizar el color de fondo
+    document.body.style.backgroundColor = colorAleatorio
+
+})
